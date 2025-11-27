8a2f313781e119e8797977b38c16086809bddd03
Camille Grings Silva <38230379+camillegrings@users.noreply.github.com>
2025-11-27 15:38:52 -0300

    feat: mobile menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { useState } from 'react'
 import styles from './style.module.css'
 import Link from 'next/link'
 import { useTranslations } from 'next-intl';
@@ -6,18 +9,33 @@ import logo from '@assets/logo.png'
 
 export default function Header() {
     const translation = useTranslations('Header');
+    const [isOpen, setIsOpen] = useState(false)
+
+    function toggleNav() {
+        setIsOpen((prev) => !prev)
+    }
 
     return (
         <header className={`${styles.container}`}>
+            <button className={`${styles.mobileMenu}`} onClick={toggleNav}>
+                <div className={`${styles.hamburger} ${isOpen && styles.active}`}>
+                    <div className={`${styles.bar} ${styles.top}`}></div>
+                    <div className={`${styles.bar} ${styles.middle}`}></div>
+                    <div className={`${styles.bar} ${styles.bottom}`}></div>
+                </div>
+            </button>
             <Link href="/">
                 <Image alt='SGI' src={logo} width={50} height={33} />
             </Link>
-            <nav className={`${styles.nav}`}>
+            <nav className={`${styles.nav} ${isOpen && styles.open}`}>
                 <Link href="/register">{translation('register')}</Link>
                 <Link href="/info">{translation('informations')}</Link>
                 <Link href="/schedule">{translation('schedule')}</Link>
                 <Link href="/#about">{translation('about')}</Link>
-                <a className={`${styles.instagram}`} href='https://www.instagram.com/sgi_oficial/' />
+                <a className={`${styles.instagram}`} href='https://www.instagram.com/sgi_oficial/'>
+                    <span className={`${styles.instagramIcon}`}></span>
+                    <span className={`${styles.instagramLabel}`}>Instagram</span>
+                </a>
             </nav>
         </header>
     );
